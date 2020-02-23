17de77ac73d31e703ce2f40512064a00f41a5a2b
AlenaK06 <54857769+AlenaK06@users.noreply.github.com>
2020-02-23 15:12:53 -0500

    adding mood iteration 1

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 const http = require('http');
 const express = require('./node_modules/express'); //de facto node framework
 const MongoClient = require('./node_modules/mongodb').MongoClient; //database
-const bodyParser= require('body-parser'); //handles reading data from forms
-const hbs = require('hbs'); //templating engine
+const bodyParser= require('./node_modules/body-parser'); //handles reading data from forms
+const hbs = require('./node_modules/hbs/lib/hbs'); //templating engine
+
 const hostname = '127.0.0.1';
 const port = 3000;
-
-
 
 const app = express();
 var db;
 
-// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-//   if (err) return console.log(err);
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) return console.log(err);
 
 
-//   db = client.db('Companion'); //Sets the database to work with
+  db = client.db('Companion'); //Sets the database to work with
 
-//   //starts a server
-//     app.listen(port, hostname, () =>{ 
-//         console.log(`Server running at http://$localhost:$3000/`);
-//     });
-// })
+  //starts a server
+    app.listen(port, hostname, () =>{ 
+        console.log(`Server running at http://$localhost:$3000/`);
+    });
+})
 
-MongoClient.connect('mongodb+srv://User1:i4QWO7qACRSlr6r8@companion-okgix.azure.mongodb.net/', function(err, db) {
+// localhost:27017
+MongoClient.connect('mongodb+srv://User2:a1QaehxEiQl0xSts@companion-okgix.azure.mongodb.net/', function(err, db) {
     if (err) throw err;
     var dbo = db.db("Companion");
-    var myobj = [
-      { mood: 'defeated'},
-      { mood: 'depressed'},
-      { mood: 'anxious'}
-    ];
-    dbo.collection("Moods").insertMany(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("Number of documents inserted: " + res.insertedCount);
-      db.close();
-    });
+    // var myobj = [
+    //   { mood: 'happy'},
+    //   { mood: 'anxious'},
+    //   { mood: 'nervous'}
+    // ];
+    // dbo.collection("Moods").insertMany(myobj, function(err, res) {
+    //   if (err) throw err;
+    //   console.log("Number of documents inserted: " + res.insertedCount);
+    //   db.close();
+    // });
 
     app.listen(port, hostname, () =>{ 
         console.log(`Server running at http://$localhost:$3000/`);
@@ -43,17 +43,32 @@ MongoClient.connect('mongodb+srv://User1:i4QWO7qACRSlr6r8@companion-okgix.azure.
 
   });
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('viewEngine', 'hbs' );
 
+app.get('/', (req, res) => {
 
-// const hostname = '127.0.0.1';
-// const port = 3000;
+    //read all notes from the Notes collection
+    db.collection('Moods').find().toArray((err, result) => {
+      if (err) return console.log(err)
+  
+      //To pass variables to a view, include an object as a second parameter. Here we pass "result" data. 
+      //The view will reference it as "notes"
+      res.render('index.hbs', {moods: result}) //by default, hbs views are placed in a "views" folder. 
+    })
+  })
+  
+  app.get('/add-mood', (req, res) => {
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://$localhost:$3000/`);
-// });
+    res.render('add-mood.hbs'); //by default, hbs views are placed in a "views" folder
+ })
+ 
+ //Saves form data and redirects back to home page.
+ app.post('/moods', (req, res) => {
+    db.collection('Moods').insertOne(req.body, (err, result) => {
+     if (err) return console.log(err)
+ 
+     console.log('saved to database') //debug console message
+     res.redirect('/')
+   })
+ })
