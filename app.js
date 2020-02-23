25648a2c12683c04b0593a44f4e9071c1bcdd631
AlenaK06 <54857769+AlenaK06@users.noreply.github.com>
2020-02-23 13:54:42 -0500

    updated app.js 1

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const http = require('http');
 const express = require('./node_modules/express'); //de facto node framework
+const MongoClient = require('./node_modules/mongodb').MongoClient; //database
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
 
 const app = express();
 var db;
 
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-  if (err) return console.log(err);
+// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+//   if (err) return console.log(err);
 
 
-  db = client.db('NotesApp'); //Sets the database to work with
+//   db = client.db('Companion'); //Sets the database to work with
 
-  //starts a server
-  app.listen(3000, () => {
-    console.log('listening on port 3000')
-  })
-})
+//   //starts a server
+//     app.listen(port, hostname, () =>{ 
+//         console.log(`Server running at http://$localhost:$3000/`);
+//     });
+// })
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect('mongodb://localhost:27017', function(err, db) {
     if (err) throw err;
     var dbo = db.db("Companion");
     var myobj = [
@@ -24,11 +29,16 @@ MongoClient.connect(url, function(err, db) {
       { mood: 'anxious'},
       { mood: 'nervous'}
     ];
-    dbo.collection("moods").insertMany(myobj, function(err, res) {
+    dbo.collection("Moods").insertMany(myobj, function(err, res) {
       if (err) throw err;
       console.log("Number of documents inserted: " + res.insertedCount);
       db.close();
     });
+
+    app.listen(port, hostname, () =>{ 
+        console.log(`Server running at http://$localhost:$3000/`);
+    });
+
   });
 
 
